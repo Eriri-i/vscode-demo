a2f60dd16290b1ac6a43571c047197773a38f0ad
TSJ <642195945@qq.com>
2023-05-06 21:14:02 +0800

    init:

---
+++ src/main/java/cn/tsj/vscodedemo/VscodeDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.tsj.vscodedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VscodeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VscodeDemoApplication.class, args);
+	}
+
+}
